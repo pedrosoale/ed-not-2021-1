47f2f98a5c591de8866f055db80851ffd6dc0797
pedrosoale <profe.pedroso@gmail.com>
2021-05-18 01:04:23 +0000

    Aulas de Estrutura de Dados

--- 15-pilha1.mjs
+++ 15-pilha1.mjs
@@ -6,15 +6,16 @@ const pilha = []
 for(let i = 0; i < texto.length; i++){
     pilha.push(texto.charAt(i))
 }
-console.log(pilha)
 
 // Inserindo alguns caracteres no MEIO da pilha (PROIBIDO!)
-//pilha.splice(16, 0, 'b', 'a', 't', 'a', 't', 'a')
+pilha.splice(16, 0, 'b', 'a', 't', 'a', 't', 'a')
+
+console.log(pilha)
 
 let textoRev = ''
 
 // Desempilhamento
-while(pilha.length){
+while(pilha.length){     // 0 é avaliado como false; qq coisa !== 0 é avaliado como true
     textoRev += pilha.pop()
 }
 console.log(textoRev)
